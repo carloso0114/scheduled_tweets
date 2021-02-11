e4c9091879a0501738a0aa1261c358a13527dd33
carloso0114 <carlossospinag@gmail.com>
2021-02-10 23:54:39 -0500

    add sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: "main#index"
   get "about-us", to: "about#index", as: :about
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 end
 
