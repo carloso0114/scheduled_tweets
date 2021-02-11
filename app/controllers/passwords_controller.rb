cd4d3caa9534523fab416f405610a32207500b0a
carloso0114 <carlossospinag@gmail.com>
2021-02-11 17:29:11 -0500

    login and logout

---
+++ app/controllers/passwords_controller.rb
@@ -0,0 +1,11 @@
+class PasswordsCOntroller < ApplicationCOntroller
+    before_action :require_user_logged_in!
+
+    def edit
+
+    end
+
+    def update
+        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
   root to: "main#index"
+
   get "about-us", to: "about#index", as: :about
+
+  get "password", to:"passwords#edit"
+  patch "password", to:"passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 end
 
