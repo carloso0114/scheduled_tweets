93af15efabca515be80dd5b8a4d8a290df76417e
carloso0114 <carlossospinag@gmail.com>
2021-02-13 16:58:23 -0500

    part 24 omniAuth urls

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,7 @@ Rails.application.routes.draw do
 
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
+
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
 end
 
