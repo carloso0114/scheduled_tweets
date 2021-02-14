5ea49325932d2cb59ec7aaa207a8a0d57f6f5736
carloso0114 <carlossospinag@gmail.com>
2021-02-13 20:11:19 -0500

    part 25 twitter account_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts
     has_secure_password
     validates :email, presence: true, format: {with:/[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9.]+/, message:"must be a valid email address"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_221834) do
+ActiveRecord::Schema.define(version: 2021_02_13_235724) do
+
+  create_table "twitter_accounts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "name"
+    t.string "username"
+    t.string "image"
+    t.string "token"
+    t.string "secret"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_twitter_accounts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_02_10_221834) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "twitter_accounts", "users"
 end
