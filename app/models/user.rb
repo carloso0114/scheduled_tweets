4d76fb49a1df827b0c6e31917897fe7a259a0376
carloso0114 <carlossospinag@gmail.com>
2021-02-18 12:12:06 -0500

    update validations in twitter and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_many :twitter_accounts
     has_many :tweets
     has_secure_password
-    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end
